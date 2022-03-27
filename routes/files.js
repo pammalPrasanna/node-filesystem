5af27e1f0e04f2e3debc83fc5f25026965f4dc78
pammalprasanna <pammalprasanna@gmail.com>
2022-03-27 17:30:19 +0530

    File system completed

---
+++ routes/files.js
@@ -0,0 +1,43 @@
+var express = require('express');
+const { route } = require('../app');
+var router = express.Router();
+
+const fs = require('fs')
+
+
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+
+const folderPath = 'files/';
+
+res.send({
+  files_list: fs.readdirSync(folderPath)
+})
+});
+
+
+router.post('/', function(req, res, next) {
+  const fileName = `${new Date().getDate()+new Date().getMonth()+new Date().getUTCFullYear()}-${new Date().getTime()}`;
+
+  const content = `${new Date()}`;
+
+  fs.writeFile(`files/${fileName}.txt`, content, err => {
+    if (err) {
+      console.error(err);
+      res.status(500)
+      res.send({
+        message: `Try again later!`
+      })
+    }
+    res.status(200);
+    res.send({
+      message: `Successfully created file with name ${fileName}! `
+    })
+  })
+  
+});
+
+module.exports = router;
